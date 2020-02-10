acaaf793db04ecd4fe71af724acef29fca2e4e34
Luke Zbihlyj <me@lukezbihlyj.co.uk>
2020-02-10 13:25:26 -0800

    Update iOS pod to >= 5.0.0 and apply migrations for native plugin code.

--- GoogleSignInPlugin/Assets/Plugins/iOS/GoogleSignIn/GoogleSignInAppController.mm
+++ GoogleSignInPlugin/Assets/Plugins/iOS/GoogleSignIn/GoogleSignInAppController.mm
@@ -75,7 +75,7 @@ GoogleSignInHandler *gsiHandler;
   // Setup the Sign-In instance.
   GIDSignIn *signIn = [GIDSignIn sharedInstance];
   signIn.clientID = clientId;
-  signIn.uiDelegate = gsiHandler;
+  signIn.presentingViewController = gsiHandler;
   signIn.delegate = gsiHandler;
 
   // looks like it's just calling itself, but the implementations were swapped
@@ -96,9 +96,7 @@ GoogleSignInHandler *gsiHandler;
                                sourceApplication:sourceApplication
                                       annotation:annotation];
 
-  return [[GIDSignIn sharedInstance] handleURL:url
-                             sourceApplication:sourceApplication
-                                    annotation:annotation] ||
+  return [[GIDSignIn sharedInstance] handleURL:url] ||
          handled;
 }
 
@@ -112,12 +110,7 @@ GoogleSignInHandler *gsiHandler;
   BOOL handled =
       [self GoogleSignInAppController:app openURL:url options:options];
 
-  return [[GIDSignIn sharedInstance]
-                     handleURL:url
-             sourceApplication:
-                 options[UIApplicationOpenURLOptionsSourceApplicationKey]
-                    annotation:
-                        options[UIApplicationOpenURLOptionsAnnotationKey]] ||
+  return [[GIDSignIn sharedInstance] handleURL:url] ||
          handled;
 }
 
